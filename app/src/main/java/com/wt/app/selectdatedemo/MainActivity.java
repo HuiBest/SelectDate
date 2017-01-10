2ded2266006532bd0dedf941e060a004cc3f635e
wugl <gui0713@163.com>
2017-01-10 10:26:47 +0800

    update readme.md

--- app/src/main/java/com/wt/app/selectdatedemo/MainActivity.java
+++ app/src/main/java/com/wt/app/selectdatedemo/MainActivity.java
@@ -1,41 +1,63 @@
 package com.wt.app.selectdatedemo;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.wt.app.selectdatedemo.view.SelectData;
 
 
-
 public class MainActivity extends AppCompatActivity {
-    private EditText editText;
+    private EditText editText1,editText2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = (EditText) findViewById(R.id.editText);
 
-        editText.setOnClickListener(new View.OnClickListener() {
+        editText1 = (EditText) findViewById(R.id.edit_text1);
+
+
+        editText1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(editText.getWindowToken(),0);
+
                 SelectData selectData  = new SelectData(MainActivity.this,true);
-                selectData.showAtLocation(editText, Gravity.BOTTOM,0,0);
+                selectData.showAtLocation(editText1, Gravity.BOTTOM,0,0);
                 selectData.setDateClickListener(new SelectData.OnDateClickListener() {
                     @Override
                     public void onClick(String year, String month, String day, String hour, String minute) {
                         Toast.makeText(MainActivity.this,
                                 year + "-" + month + "-" + day+" "+hour+":"+minute,
                                 Toast.LENGTH_LONG).show();
-                        editText.setText(year + "-" + month + "-" + day+" "+hour+":"+minute);
+                        editText1.setText(year + "-" + month + "-" + day+" "+hour+":"+minute);
+
+                    }
+                });
+            }
+        });
+
+
+
+        editText2 = (EditText) findViewById(R.id.edit_text2);
+
+        editText2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SelectData selectData  = new SelectData(MainActivity.this,false);
+                selectData.showAtLocation(editText2, Gravity.BOTTOM,0,0);
+                selectData.setDateClickListener(new SelectData.OnDateClickListener() {
+                    @Override
+                    public void onClick(String year, String month, String day, String hour, String minute) {
+                        Toast.makeText(MainActivity.this,
+                                year + "-" + month + "-" + day,
+                                Toast.LENGTH_LONG).show();
+                        editText2.setText(year + "-" + month + "-" + day);
 
                     }
                 });
